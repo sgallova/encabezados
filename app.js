e81da15cc9f933af7191d0af0e6bd7c8ef651429
Sofia <sofigava@gmail.com>
2021-04-06 20:51:03 -0500

    validacion nulo

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 const parser = require('ua-parser-js');
+let msg='';
 
 
 app.get('/',(req,res) =>{
     var ua = parser(req.headers['user-agent']);
+    if(ua.browser.name=== ''){
+        res.send('mi-super-navegador');
+    }else{
     res.send(ua.browser.name);
+    }
 });
 
 
